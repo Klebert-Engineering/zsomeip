a2e09da67443f7f9d94c29785998db68468621c8
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2022-03-16 19:53:55 +0100

    defs: Add constructor to AgentDefinition.

--- definitions/include/zsomeip-defs/AgentDefinition.h
+++ definitions/include/zsomeip-defs/AgentDefinition.h
@@ -10,6 +10,9 @@ struct AgentDefinition {
     /* SOME/IP agent identifier - must match configuration file! */
     const vsomeip::eventgroup_t serviceId{};
     const vsomeip::instance_t instanceId{};
+
+    AgentDefinition(vsomeip::eventgroup_t s, vsomeip::instance_t i)
+    : serviceId(s), instanceId(i) {}
 };
 
 }
